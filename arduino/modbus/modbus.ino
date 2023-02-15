ad0cb753628eab82bbb7ec2e913ab793c53ce6fb
zatalian <zatalian@users.noreply.github.com>
2023-02-15 17:26:38 +0100

    Add files via upload

---
+++ arduino/modbus/modbus.ino
@@ -0,0 +1,47 @@
+#include "ModbusSlave.h"
+
+#define SLAVE_ID 1
+#define CTRL_PIN 0
+#define BAUDRATE 115200
+
+Modbus slave(SLAVE_ID, CTRL_PIN);
+
+void setup()
+{
+  for (int i = 2; i < 8; i++)
+    pinMode(i, INPUT_PULLUP);
+
+  for (int i = 8; i < 14; i++)
+    pinMode(i, OUTPUT); 
+  
+  slave.cbVector[CB_READ_DISCRETE_INPUTS] = readDigitalIn;
+  slave.cbVector[CB_WRITE_COILS] = writeDigitalOut;
+  
+  Serial.begin(BAUDRATE, SERIAL_8E1);
+  slave.begin(BAUDRATE);
+}
+
+void loop()
+{
+  slave.poll();
+}
+
+uint8_t readDigitalIn(uint8_t fc, uint16_t offset, uint16_t length)
+{
+  if (offset + length <= 6)
+    for (int i = 0; i < length; i++)
+      slave.writeCoilToBuffer(i, digitalRead(offset + i + 2));
+  else return STATUS_ILLEGAL_DATA_ADDRESS;
+        
+  return STATUS_OK;
+}
+
+uint8_t writeDigitalOut(uint8_t fc, uint16_t offset, uint16_t length)
+{
+  if (offset + length <= 6)
+    for (int i = 0; i < length; i++)
+      digitalWrite(offset + i + 8, slave.readCoilFromBuffer(i));
+  else return STATUS_ILLEGAL_DATA_ADDRESS;
+  
+  return STATUS_OK;
+}
